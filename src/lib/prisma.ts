0600407afb6ac72705b16072fea3f0328518fe4b
Roxy <roxy@Roxys-MacBook-Pro.local>
2026-07-09 15:54:38 +0600

    coment update

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,13 @@
 import { PrismaClient } from '@prisma/client';
 
+
+// Create a new instance of the Prisma client
 const prisma = new PrismaClient({
   log: process.env.NODE_ENV === 'development' ? ['warn', 'error'] : ['error'],
 });
 
+
+
+
+// Export the Prisma client instance
 export default prisma;
