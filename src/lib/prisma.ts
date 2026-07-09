5ee454f9081a8d811582fee0a89ecc6f91b41a28
Roxy <roxy@Roxys-MacBook-Pro.local>
2026-07-09 19:29:28 +0600

    sripe add

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 
-
-// Create a new instance of the Prisma client
 const prisma = new PrismaClient({
   log: process.env.NODE_ENV === 'development' ? ['warn', 'error'] : ['error'],
 });
 
-
-
-
-// Export the Prisma client instance
 export default prisma;
